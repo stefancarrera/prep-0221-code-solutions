9e8b1e7f40e2990732d49d5b480d7baf19697266
Stefan Carrera <stefancarrera@gmail.com>
2021-02-27 08:32:31 -0600

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,21 @@
+var n = ' ';
+
+for (var x = 0; x <= 9; x++) {
+  n = n + x;
+}
+console.log(n);
+
+console.log('Javascript loop 1');
+for (var z = 0; z <= 9; z++) {
+  console.log(z);
+}
+
+console.log('Javascript loop 2');
+for (var g = 0; g < 20; g += 2) {
+  console.log(g);
+}
+
+console.log('Javascript loop 3');
+for (var b = 100; b >= 0; b--) {
+  console.log('Time till explosion: ' + b + '!');
+}
